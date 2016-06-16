40bfc3d335febcbb7f959ccbccf8dd969f3af878
Gustavo Bustos <runscible@gmail.com>
2016-06-16 11:57:35 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$('document').ready(function(){
+	var numero = _.random(1,42);
+	//alert(numero);
+	for( variable = 0; variable < 41; variable = _.random(1, 42)){	 
+	console.log(variable);
+		}
+	});
